48830a13edadd121aaade30ad0c19951829489b1
Jack Matthews <jack.matthews@origin.com.au>
2019-09-04 12:22:04 +1000

    Call shadowJar from deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_995a2870b5b0_key -iv $encrypted_995a2870b5b0_iv -in sonar.travis.gpg.enc -out sonar.travis.gpg -d
   echo 'Signing & Deploying to Maven Central'
-  ./gradlew signArchives uploadArchives -PossrhUsername=${SONAR_USERNAME} -PossrhPassword=${SONAR_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=../sonar.travis.gpg
+  ./gradlew shadowJar signArchives uploadArchives -PossrhUsername=${SONAR_USERNAME} -PossrhPassword=${SONAR_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=../sonar.travis.gpg
 fi
