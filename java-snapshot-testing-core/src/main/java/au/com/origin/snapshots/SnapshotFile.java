d8d63d81f845350562968814e7d5af206b0f8718
Jack Matthews <6348088+jackmatt2@users.noreply.github.com>
2022-09-14 22:53:14 +1000

    Revert "Fixes #91: Debug file add all snapshots (even the successful ones) (#97)"
    This reverts commit 001b20b904429e77f642d403a0f76e765a06d093.

--- java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotFile.java
+++ java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotFile.java
@@ -4,19 +4,13 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
-import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.BiFunction;
@@ -33,7 +27,6 @@ public class SnapshotFile {
   private final BiFunction<Class<?>, String, String> onSaveSnapshotFile;
   @Getter
   private Set<String> rawSnapshots;
-  private Set<String> rawDebugSnapshots = Collections.synchronizedSortedSet(new TreeSet<>());
 
   SnapshotFile(String srcDirPath, String fileName, Class<?> testClass, BiFunction<Class<?>, String, String> onSaveSnapshotFile) throws IOException {
     this.testClass = testClass;
@@ -64,12 +57,29 @@ public class SnapshotFile {
     } catch (IOException e) {
       rawSnapshots = Collections.synchronizedSortedSet(new TreeSet<>());
     }
-
-    deleteDebugFile();
   }
 
   private String getDebugFilename() {
     return this.fileName + ".debug";
+  }
+
+  public File createDebugFile(String snapshot) {
+    File file = null;
+    try {
+      file = new File(getDebugFilename());
+      file.getParentFile().mkdirs();
+      file.createNewFile();
+
+      try (FileOutputStream fileStream = new FileOutputStream(file, false)) {
+        fileStream.write(snapshot.getBytes(StandardCharsets.UTF_8));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return file;
   }
 
   @SneakyThrows
@@ -83,8 +93,8 @@ public class SnapshotFile {
   }
 
   @SneakyThrows
-  public synchronized File createFileIfNotExists(String filename) {
-    Path path = Paths.get(filename);
+  public synchronized File createFileIfNotExists() {
+    Path path = Paths.get(this.fileName);
     if (!Files.exists(path)) {
       Files.createDirectories(path.getParent());
       Files.createFile(path);
@@ -92,18 +102,11 @@ public class SnapshotFile {
     return path.toFile();
   }
 
-  public void pushSnapshot(String snapshot) {
+  public void push(String snapshot) {
     rawSnapshots.add(snapshot);
-    updateFile(this.fileName, rawSnapshots);
-  }
 
-  public void pushDebugSnapshot(String snapshot) {
-    rawDebugSnapshots.add(snapshot);
-    updateFile(getDebugFilename(), rawDebugSnapshots);
-  }
+    File file = createFileIfNotExists();
 
-  private void updateFile(String fileName, Set<String> rawSnapshots) {
-    File file = createFileIfNotExists(fileName);
     try (FileOutputStream fileStream = new FileOutputStream(file, false)) {
       byte[] myBytes = String.join(SPLIT_STRING, rawSnapshots).getBytes(StandardCharsets.UTF_8);
       fileStream.write(myBytes);
@@ -116,11 +119,8 @@ public class SnapshotFile {
   public void cleanup() {
     Path path = Paths.get(this.fileName);
     if (Files.exists(path)) {
-      if (Files.size(path) == 0 || snapshotsAreTheSame()) {
-        deleteDebugFile();
-      }
-
       if (Files.size(path) == 0) {
+        deleteDebugFile();
         delete();
       } else {
         String content = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
@@ -128,17 +128,5 @@ public class SnapshotFile {
         Files.write(path, modified.getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING);
       }
     }
-  }
-
-  @SneakyThrows
-  private boolean snapshotsAreTheSame() {
-    Path path = Paths.get(this.getDebugFilename());
-    if (Files.exists(path)) {
-      String snapshotFileContent = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
-      String debugSnapshotFileContent = new String(Files.readAllBytes(Paths.get(this.getDebugFilename())), StandardCharsets.UTF_8);
-      return Objects.equals(snapshotFileContent, debugSnapshotFileContent);
-    }
-
-    return false;
   }
 }
