origin-energy/java-snapshot-testing

commit 001b20b904429e77f642d403a0f76e765a06d093

Fixes #91: Debug file add all snapshots (even the successful ones) (#97)
Co-authored-by: Mariusz Miesiak <mariusz.miesiak@tymit.io>

java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotFile.java
@@ -4,13 +4,19 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.BiFunction;
@@ -27,6 +33,7 @@ public class SnapshotFile {
   private final BiFunction<Class<?>, String, String> onSaveSnapshotFile;
   @Getter
   private Set<String> rawSnapshots;
+  private Set<String> rawDebugSnapshots = Collections.synchronizedSortedSet(new TreeSet<>());
 
   SnapshotFile(String srcDirPath, String fileName, Class<?> testClass, BiFunction<Class<?>, String, String> onSaveSnapshotFile) throws IOException {
     this.testClass = testClass;
@@ -57,29 +64,12 @@ public class SnapshotFile {
     } catch (IOException e) {
       rawSnapshots = Collections.synchronizedSortedSet(new TreeSet<>());
     }
+
+    deleteDebugFile();
   }
 
   private String getDebugFilename() {
     return this.fileName + ".debug";
-  }
-
-  public File createDebugFile(String snapshot) {
-    File file = null;
-    try {
-      file = new File(getDebugFilename());
-      file.getParentFile().mkdirs();
-      file.createNewFile();
-
-      try (FileOutputStream fileStream = new FileOutputStream(file, false)) {
-        fileStream.write(snapshot.getBytes(StandardCharsets.UTF_8));
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    return file;
   }
 
   @SneakyThrows
@@ -93,8 +83,8 @@ public class SnapshotFile {
   }
 
   @SneakyThrows
-  public synchronized File createFileIfNotExists() {
-    Path path = Paths.get(this.fileName);
+  public synchronized File createFileIfNotExists(String filename) {
+    Path path = Paths.get(filename);
     if (!Files.exists(path)) {
       Files.createDirectories(path.getParent());
       Files.createFile(path);
@@ -102,11 +92,18 @@ public class SnapshotFile {
     return path.toFile();
   }
 
-  public void push(String snapshot) {
+  public void pushSnapshot(String snapshot) {
     rawSnapshots.add(snapshot);
+    updateFile(this.fileName, rawSnapshots);
+  }
 
-    File file = createFileIfNotExists();
+  public void pushDebugSnapshot(String snapshot) {
+    rawDebugSnapshots.add(snapshot);
+    updateFile(getDebugFilename(), rawDebugSnapshots);
+  }
 
+  private void updateFile(String fileName, Set<String> rawSnapshots) {
+    File file = createFileIfNotExists(fileName);
     try (FileOutputStream fileStream = new FileOutputStream(file, false)) {
       byte[] myBytes = String.join(SPLIT_STRING, rawSnapshots).getBytes(StandardCharsets.UTF_8);
       fileStream.write(myBytes);
@@ -119,8 +116,11 @@ public class SnapshotFile {
   public void cleanup() {
     Path path = Paths.get(this.fileName);
     if (Files.exists(path)) {
-      if (Files.size(path) == 0) {
+      if (Files.size(path) == 0 || snapshotsAreTheSame()) {
         deleteDebugFile();
+      }
+
+      if (Files.size(path) == 0) {
         delete();
       } else {
         String content = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
@@ -128,5 +128,17 @@ public class SnapshotFile {
         Files.write(path, modified.getBytes(StandardCharsets.UTF_8), StandardOpenOption.TRUNCATE_EXISTING);
       }
     }
+  }
+
+  @SneakyThrows
+  private boolean snapshotsAreTheSame() {
+    Path path = Paths.get(this.getDebugFilename());
+    if (Files.exists(path)) {
+      String snapshotFileContent = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
+      String debugSnapshotFileContent = new String(Files.readAllBytes(Paths.get(this.getDebugFilename())), StandardCharsets.UTF_8);
+      return Objects.equals(snapshotFileContent, debugSnapshotFileContent);
+    }
+
+    return false;
   }
 }

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/DebugSnapshotTest.java
@@ -52,41 +52,41 @@ public class DebugSnapshotTest {
     assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
   }
 
-  @DisplayName("Debug file should not be created when snapshots match for a new snapshot")
+  @DisplayName("Debug file should be created when snapshots match for a new snapshot")
   @Test
-  void noDebugFileCreatedNewSnapshot(TestInfo testInfo) {
+  void debugFileCreatedNewSnapshot(TestInfo testInfo) {
     assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
 
     SnapshotVerifier snapshotVerifier = new SnapshotVerifier(DEFAULT_CONFIG, testInfo.getTestClass().get());
     Expect expect = Expect.of(snapshotVerifier, testInfo.getTestMethod().get());
     assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
     expect.toMatchSnapshot(new TestObjectGood());
-    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+    assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
   }
 
-  @DisplayName("Debug file should not be created when snapshots match for an existing snapshot")
+  @DisplayName("Debug file should be created when snapshots match for an existing snapshot")
   @Test
-  void noDebugFileCreatedExistingSnapshot(TestInfo testInfo) {
+  void debugFileCreatedExistingSnapshot(TestInfo testInfo) {
     assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
 
     SnapshotVerifier snapshotVerifier = new SnapshotVerifier(DEFAULT_CONFIG, testInfo.getTestClass().get());
     Expect expect = Expect.of(snapshotVerifier, testInfo.getTestMethod().get());
     assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
     expect.toMatchSnapshot(new TestObjectGood());
-    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+    assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
   }
 
   @SneakyThrows
-  @DisplayName("existing debug file should be deleted once snapshots match")
+  @DisplayName("Existing debug file should not be deleted once snapshots match")
   @Test
-  void deleteDebugFile(TestInfo testInfo) {
+  void debugFileCreatedSnapshotMatch(TestInfo testInfo) {
     assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
     Files.createFile(Paths.get(DEBUG_FILE_PATH));
     assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
     SnapshotVerifier snapshotVerifier = new SnapshotVerifier(DEFAULT_CONFIG, testInfo.getTestClass().get());
     Expect expect = Expect.of(snapshotVerifier, testInfo.getTestMethod().get());
     expect.toMatchSnapshot(new TestObjectGood());
-    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+    assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
   }
 
   private class TestObjectBad {

java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/Snapshot.java
@@ -53,13 +53,10 @@ public class Snapshot {
     this.scenario = null;
   }
 
-
   public void toMatchSnapshot() {
-
     Set<String> rawSnapshots = snapshotFile.getRawSnapshots();
 
     String rawSnapshot = getRawSnapshot(rawSnapshots);
-
     String currentObject = takeSnapshot();
 
     if (rawSnapshot != null && shouldUpdateSnapshot()) {
@@ -68,10 +65,10 @@ public class Snapshot {
     }
 
     if (rawSnapshot != null) {
+      snapshotFile.pushDebugSnapshot(currentObject.trim());
+
       // Match existing Snapshot
       if (!snapshotComparator.matches(getSnapshotName(), rawSnapshot, currentObject)) {
-        snapshotFile.createDebugFile(currentObject.trim());
-
         List<SnapshotReporter> reporters = snapshotReporters
             .stream()
             .filter(reporter -> reporter.supportsFormat(snapshotSerializer.getOutputFormat()))
@@ -103,10 +100,10 @@ public class Snapshot {
       } else {
         log.warn("We detected you are running on a developer machine - if this is incorrect please override the isCI() method in SnapshotConfig");
         // Create New Snapshot
-        snapshotFile.push(currentObject);
+        snapshotFile.pushSnapshot(currentObject);
+        snapshotFile.pushDebugSnapshot(currentObject.trim());
       }
     }
-    snapshotFile.deleteDebugFile();
   }
 
   private boolean shouldUpdateSnapshot() {

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/EmptySnapshotFileTest.java
@@ -27,7 +27,7 @@ public class EmptySnapshotFileTest {
     SnapshotVerifier snapshotVerifier = new SnapshotVerifier(new ToStringSnapshotConfig(), testInfo.getTestClass().get());
     snapshotVerifier.validateSnapshots();
 
-    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+    assertTrue(Files.notExists(Paths.get(SNAPSHOT_FILE_PATH)));
     assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
   }
 

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/EqualDebugSnapshotFileTest.java
@@ -1,0 +1,38 @@
+package au.com.origin.snapshots;
+
+import au.com.origin.snapshots.config.ToStringSnapshotConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EqualDebugSnapshotFileTest {
+
+  private static final String SNAPSHOT_FILE_PATH = "src/test/java/au/com/origin/snapshots/__snapshots__/EqualDebugSnapshotFileTest.snap";
+  private static final String DEBUG_FILE_PATH = "src/test/java/au/com/origin/snapshots/__snapshots__/EqualDebugSnapshotFileTest.snap.debug";
+
+  @BeforeAll
+  static void beforeAll() {
+    SnapshotUtils.copyTestSnapshots();
+  }
+
+  @DisplayName("Should remove equal debug snapshots")
+  @Test
+  public void shouldRemoveEmptySnapshots(TestInfo testInfo) {
+    assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
+    assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
+
+    SnapshotVerifier snapshotVerifier = new SnapshotVerifier(new ToStringSnapshotConfig(), testInfo.getTestClass().get());
+    snapshotVerifier.validateSnapshots();
+
+    assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
+    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+  }
+
+}

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/SnapshotTest.java
@@ -72,7 +72,7 @@ class SnapshotTest {
 
   @Test
   void shouldMatchSnapshotWithException() {
-    snapshotFile.push(SNAPSHOT_NAME + "anyWrongSnapshot");
+    snapshotFile.pushSnapshot(SNAPSHOT_NAME + "anyWrongSnapshot");
 
     assertThrows(SnapshotMatchException.class, snapshot::toMatchSnapshot);
   }
@@ -129,7 +129,7 @@ class SnapshotTest {
     snapshot.setScenario("hello world");
     snapshot.toMatchSnapshot();
     Mockito.verify(snapshotFile)
-        .push("java.lang.String.toString[hello world]=[\nanyObject\n]");
+        .pushSnapshot("java.lang.String.toString[hello world]=[\nanyObject\n]");
   }
 
   @SneakyThrows

commit cb827a899957e887658fc7fa30e495abf22bfb22

Fixes #99: Fix line ending handling problem introduced in PR #97

java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotFile.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
@@ -134,8 +135,8 @@ public class SnapshotFile {
   private boolean snapshotsAreTheSame() {
     Path path = Paths.get(this.getDebugFilename());
     if (Files.exists(path)) {
-      String snapshotFileContent = new String(Files.readAllBytes(Paths.get(this.fileName)), StandardCharsets.UTF_8);
-      String debugSnapshotFileContent = new String(Files.readAllBytes(Paths.get(this.getDebugFilename())), StandardCharsets.UTF_8);
+      List<String> snapshotFileContent = Files.readAllLines(Paths.get(this.fileName), StandardCharsets.UTF_8);
+      List<String> debugSnapshotFileContent = Files.readAllLines(Paths.get(this.getDebugFilename()), StandardCharsets.UTF_8);
       return Objects.equals(snapshotFileContent, debugSnapshotFileContent);
     }
 

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/DebugSnapshotLineEndingsTest.java
@@ -1,0 +1,84 @@
+package au.com.origin.snapshots;
+
+import au.com.origin.snapshots.config.BaseSnapshotConfig;
+import au.com.origin.snapshots.serializers.SerializerType;
+import au.com.origin.snapshots.serializers.SnapshotSerializer;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DebugSnapshotLineEndingsTest {
+  private static final SnapshotConfig CONFIG = new BaseSnapshotConfig() {
+    @Override
+    public SnapshotSerializer getSerializer() {
+      return new MultiLineSnapshotSerializer();
+    }
+  };
+  private static final String DEBUG_FILE_PATH = "src/test/java/au/com/origin/snapshots/__snapshots__/DebugSnapshotLineEndingsTest.snap.debug";
+  private static final String SNAPSHOT_FILE_PATH = "src/test/java/au/com/origin/snapshots/__snapshots__/DebugSnapshotLineEndingsTest.snap";
+
+  @BeforeAll
+  static void beforeAll() {
+    SnapshotUtils.copyTestSnapshots();
+  }
+
+  @SneakyThrows
+  @BeforeEach
+  void beforeEach() {
+    Files.deleteIfExists(Paths.get(DEBUG_FILE_PATH));
+  }
+
+  /**
+   * Scenario:
+   * - An existing snapshot file checked out from git will have CR LF line endings on Windows OS
+   * - A newly created snapshot file will have LF line endings on any OS (see MultiLineSnapshotSerializer)
+   * Expectation:
+   * - As snapshot file content is identical (except for line endings), the debug file should not be created
+   */
+  @DisplayName("Debug file should not be created when snapshots match the existing snapshot regardless of line endings")
+  @Test
+  void existingSnapshotDifferentLineEndings(TestInfo testInfo) {
+    assertTrue(Files.exists(Paths.get(SNAPSHOT_FILE_PATH)));
+    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)));
+
+    SnapshotVerifier snapshotVerifier = new SnapshotVerifier(CONFIG, testInfo.getTestClass().get());
+    Expect expect = Expect.of(snapshotVerifier, testInfo.getTestMethod().get());
+    expect.toMatchSnapshot(Arrays.asList("a", "b"));
+    snapshotVerifier.validateSnapshots();
+
+    assertTrue(Files.notExists(Paths.get(DEBUG_FILE_PATH)), "Debug file should not be created");
+  }
+
+  private static class MultiLineSnapshotSerializer implements SnapshotSerializer {
+    @Override
+    public String getOutputFormat() {
+      return SerializerType.TEXT.name();
+    }
+
+    @Override
+    public String apply(Object[] objects) {
+      return "[\n" + Arrays.stream(objects)
+          .flatMap(object -> {
+            if (object instanceof Collection) {
+              return ((Collection<?>) object).stream();
+            }
+            return Stream.of(object);
+          })
+          .map(Object::toString)
+          .collect(Collectors.joining("\n")) +
+          "\n]";
+    }
+  }
+}

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/DebugSnapshotTest.java
@@ -89,14 +89,14 @@ public class DebugSnapshotTest {
     assertTrue(Files.exists(Paths.get(DEBUG_FILE_PATH)));
   }
 
-  private class TestObjectBad {
+  private static class TestObjectBad {
     @Override
     public String toString() {
       return "Bad Snapshot";
     }
   }
 
-  private class TestObjectGood {
+  private static class TestObjectGood {
     @Override
     public String toString() {
       return "Good Snapshot";

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/OnLoadSnapshotFileTest.java
@@ -1,14 +1,12 @@
 package au.com.origin.snapshots;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import au.com.origin.snapshots.config.BaseSnapshotConfig;
 import au.com.origin.snapshots.serializers.ToStringSnapshotSerializer;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;

java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/existing-snapshots/__snapshots__/DebugSnapshotLineEndingsTest.snap
@@ -1,0 +1,4 @@
+au.com.origin.snapshots.DebugSnapshotLineEndingsTest.existingSnapshotDifferentLineEndings=[
+a
+b
+]