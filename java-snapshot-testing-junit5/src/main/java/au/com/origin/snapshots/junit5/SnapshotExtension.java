fd78c991dc0ed38c692cdd8df1a990dba282a072
Shobhit <shobhit.kumar@phonepe.com>
2023-04-19 11:04:34 +0530

    removed failure in case of failed tests

--- java-snapshot-testing-junit5/src/main/java/au/com/origin/snapshots/junit5/SnapshotExtension.java
+++ java-snapshot-testing-junit5/src/main/java/au/com/origin/snapshots/junit5/SnapshotExtension.java
@@ -7,8 +7,10 @@ import au.com.origin.snapshots.config.SnapshotConfig;
 import au.com.origin.snapshots.config.SnapshotConfigInjector;
 import au.com.origin.snapshots.exceptions.SnapshotMatchException;
 import au.com.origin.snapshots.logging.LoggingHelper;
+
 import java.lang.reflect.Field;
 import java.util.Arrays;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.*;
 import org.junit.jupiter.engine.descriptor.ClassBasedTestDescriptor;
@@ -16,107 +18,111 @@ import org.junit.jupiter.engine.descriptor.ClassTestDescriptor;
 
 @Slf4j
 public class SnapshotExtension
-    implements AfterAllCallback,
+        implements AfterAllCallback,
         BeforeAllCallback,
         SnapshotConfigInjector,
         ParameterResolver,
         BeforeEachCallback {
 
-  private SnapshotVerifier snapshotVerifier;
+    private SnapshotVerifier snapshotVerifier;
 
-  @Override
-  public void beforeAll(ExtensionContext context) {
-    // don't fail if a test is run alone from the IDE for example
-    boolean failOnOrphans = shouldFailOnOrphans(context);
-    Class<?> testClass =
-        context
-            .getTestClass()
-            .orElseThrow(() -> new SnapshotMatchException("Unable to locate Test class"));
-    this.snapshotVerifier = new SnapshotVerifier(getSnapshotConfig(), testClass, failOnOrphans);
-  }
-
-  @Override
-  public void afterAll(ExtensionContext context) {
-    this.snapshotVerifier.validateSnapshots();
-  }
-
-  @Override
-  public SnapshotConfig getSnapshotConfig() {
-    return new PropertyResolvingSnapshotConfig();
-  }
-
-  /**
-   * FIXME This is a hack until I find the correct way to determine if a test run is individual or
-   * as part of a class
-   *
-   * @param context
-   * @return
-   */
-  private boolean shouldFailOnOrphans(ExtensionContext context) {
-    try {
-      Field field = context.getClass().getSuperclass().getDeclaredField("testDescriptor");
-      field.setAccessible(true);
-      Object testDescriptor = field.get(context);
-      if (testDescriptor instanceof ClassTestDescriptor) { // Junit 5.3.2
-        ClassTestDescriptor classTestDescriptor = (ClassTestDescriptor) testDescriptor;
-        return classTestDescriptor.getChildren().size() > 1;
-      } else if (testDescriptor instanceof ClassBasedTestDescriptor) { // Junit 5.7.2
-        ClassBasedTestDescriptor classTestDescriptor = (ClassBasedTestDescriptor) testDescriptor;
-        return classTestDescriptor.getChildren().size() > 1;
-      }
-    } catch (Exception e) {
-      log.error(
-          "FAILED: (Java Snapshot Testing) Unable to get JUnit5 ClassTestDescriptor or ClassBasedTestDescriptor!\n"
-              + "Ensure you are using Junit5 >= 5.3.2\n"
-              + "This may be due to JUnit5 changing their private api as we use reflection to access it\n"
-              + "Log a support ticket https://github.com/origin-energy/java-snapshot-testing/issues and supply your JUnit5 version\n"
-              + "Setting failOnOrphans=true as this is the safest option."
-              + "This means that running a test alone (say from the IDE) will fail the snapshot, you need to run the entire class.",
-          e);
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        // don't fail if a test is run alone from the IDE for example
+        boolean failOnOrphans = shouldFailOnOrphans(context);
+        Class<?> testClass =
+                context
+                        .getTestClass()
+                        .orElseThrow(() -> new SnapshotMatchException("Unable to locate Test class"));
+        this.snapshotVerifier = new SnapshotVerifier(getSnapshotConfig(), testClass, failOnOrphans);
     }
-    return true;
-  }
 
-  @Override
-  public boolean supportsParameter(
-      ParameterContext parameterContext, ExtensionContext extensionContext)
-      throws ParameterResolutionException {
-    boolean supports = parameterContext.getParameter().getType() == Expect.class;
-    if (supports) {
-      LoggingHelper.deprecatedV5(
-          log,
-          "Injecting 'Expect' via method a argument is no longer recommended. Consider using instance variable injection instead.");
+    @Override
+    public void afterAll(ExtensionContext context) {
+        this.snapshotVerifier.validateSnapshots();
     }
-    return supports;
-  }
 
-  @Override
-  public Object resolveParameter(
-      ParameterContext parameterContext, ExtensionContext extensionContext)
-      throws ParameterResolutionException {
-    return new Expect(
-        snapshotVerifier,
-        extensionContext
-            .getTestMethod()
-            .orElseThrow(() -> new RuntimeException("getTestMethod() is missing")));
-  }
-
-  @Override
-  public void beforeEach(ExtensionContext context) {
-    if (context.getTestInstance().isPresent() && context.getTestMethod().isPresent()) {
-      Arrays.stream(context.getTestClass().get().getDeclaredFields())
-          .filter(it -> it.getType() == Expect.class)
-          .findFirst()
-          .ifPresent(
-              field -> {
-                Expect expect = Expect.of(snapshotVerifier, context.getTestMethod().get());
-                field.setAccessible(true);
-                try {
-                  field.set(context.getTestInstance().get(), expect);
-                } catch (IllegalAccessException e) {
-                  throw new RuntimeException(e);
-                }
-              });
+    @Override
+    public SnapshotConfig getSnapshotConfig() {
+        return new PropertyResolvingSnapshotConfig();
     }
-  }
+
+    /**
+     * FIXME This is a hack until I find the correct way to determine if a test run is individual or
+     * as part of a class
+     *
+     * @param context
+     * @return
+     */
+    private boolean shouldFailOnOrphans(ExtensionContext context) {
+        try {
+            //ToDo: put this behind some flag
+            if (System.getProperty("shadowMode") == null || "true".equals(System.getProperty("shadowMode"))) {
+                return false;
+            }
+            Field field = context.getClass().getSuperclass().getDeclaredField("testDescriptor");
+            field.setAccessible(true);
+            Object testDescriptor = field.get(context);
+            if (testDescriptor instanceof ClassTestDescriptor) { // Junit 5.3.2
+                ClassTestDescriptor classTestDescriptor = (ClassTestDescriptor) testDescriptor;
+                return classTestDescriptor.getChildren().size() > 1;
+            } else if (testDescriptor instanceof ClassBasedTestDescriptor) { // Junit 5.7.2
+                ClassBasedTestDescriptor classTestDescriptor = (ClassBasedTestDescriptor) testDescriptor;
+                return classTestDescriptor.getChildren().size() > 1;
+            }
+        } catch (Exception e) {
+            log.error(
+                    "FAILED: (Java Snapshot Testing) Unable to get JUnit5 ClassTestDescriptor or ClassBasedTestDescriptor!\n"
+                            + "Ensure you are using Junit5 >= 5.3.2\n"
+                            + "This may be due to JUnit5 changing their private api as we use reflection to access it\n"
+                            + "Log a support ticket https://github.com/origin-energy/java-snapshot-testing/issues and supply your JUnit5 version\n"
+                            + "Setting failOnOrphans=true as this is the safest option."
+                            + "This means that running a test alone (say from the IDE) will fail the snapshot, you need to run the entire class.",
+                    e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean supportsParameter(
+            ParameterContext parameterContext, ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        boolean supports = parameterContext.getParameter().getType() == Expect.class;
+        if (supports) {
+            LoggingHelper.deprecatedV5(
+                    log,
+                    "Injecting 'Expect' via method a argument is no longer recommended. Consider using instance variable injection instead.");
+        }
+        return supports;
+    }
+
+    @Override
+    public Object resolveParameter(
+            ParameterContext parameterContext, ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        return new Expect(
+                snapshotVerifier,
+                extensionContext
+                        .getTestMethod()
+                        .orElseThrow(() -> new RuntimeException("getTestMethod() is missing")));
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        if (context.getTestInstance().isPresent() && context.getTestMethod().isPresent()) {
+            Arrays.stream(context.getTestClass().get().getDeclaredFields())
+                    .filter(it -> it.getType() == Expect.class)
+                    .findFirst()
+                    .ifPresent(
+                            field -> {
+                                Expect expect = Expect.of(snapshotVerifier, context.getTestMethod().get());
+                                field.setAccessible(true);
+                                try {
+                                    field.set(context.getTestInstance().get(), expect);
+                                } catch (IllegalAccessException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+        }
+    }
 }
