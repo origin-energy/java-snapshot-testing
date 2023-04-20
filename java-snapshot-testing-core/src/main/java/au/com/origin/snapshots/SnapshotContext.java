0e5c48a6881d87a5f72888e1d5dba04b86f97e9d
Shobhit <shobhit.kumar@phonepe.com>
2023-04-20 11:55:44 +0530

    moved shadowMode condition up since pushDebugSnapshot is also creating a new debug file

--- java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotContext.java
+++ java-snapshot-testing-core/src/main/java/au/com/origin/snapshots/SnapshotContext.java
@@ -8,9 +8,11 @@ import au.com.origin.snapshots.exceptions.SnapshotExtensionException;
 import au.com.origin.snapshots.exceptions.SnapshotMatchException;
 import au.com.origin.snapshots.reporters.SnapshotReporter;
 import au.com.origin.snapshots.serializers.SnapshotSerializer;
+
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -18,158 +20,166 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SnapshotContext {
 
-  private static final List<String> RESERVED_WORDS = Arrays.asList("=", "[", "]");
+    private static final List<String> RESERVED_WORDS = Arrays.asList("=", "[", "]");
 
-  private final SnapshotConfig snapshotConfig;
-  private final SnapshotFile snapshotFile;
+    private final SnapshotConfig snapshotConfig;
+    private final SnapshotFile snapshotFile;
 
-  @Getter final Class<?> testClass;
+    @Getter
+    final Class<?> testClass;
 
-  @Getter final Method testMethod;
+    @Getter
+    final Method testMethod;
 
-  final Object current;
-  private final boolean isCI;
+    final Object current;
+    private final boolean isCI;
 
-  @Setter private SnapshotSerializer snapshotSerializer;
-  @Setter private SnapshotComparator snapshotComparator;
-  @Setter private List<SnapshotReporter> snapshotReporters;
+    @Setter
+    private SnapshotSerializer snapshotSerializer;
+    @Setter
+    private SnapshotComparator snapshotComparator;
+    @Setter
+    private List<SnapshotReporter> snapshotReporters;
 
-  @Setter @Getter String scenario;
+    @Setter
+    @Getter
+    String scenario;
 
-  @Getter SnapshotHeader header = new SnapshotHeader();
+    @Getter
+    SnapshotHeader header = new SnapshotHeader();
 
-  SnapshotContext(
-      SnapshotConfig snapshotConfig,
-      SnapshotFile snapshotFile,
-      Class<?> testClass,
-      Method testMethod,
-      Object current) {
-    this.snapshotConfig = snapshotConfig;
-    this.snapshotFile = snapshotFile;
-    this.testClass = testClass;
-    this.testMethod = testMethod;
-    this.current = current;
+    SnapshotContext(
+            SnapshotConfig snapshotConfig,
+            SnapshotFile snapshotFile,
+            Class<?> testClass,
+            Method testMethod,
+            Object current) {
+        this.snapshotConfig = snapshotConfig;
+        this.snapshotFile = snapshotFile;
+        this.testClass = testClass;
+        this.testMethod = testMethod;
+        this.current = current;
 
-    this.isCI = snapshotConfig.isCI();
-    this.snapshotSerializer = snapshotConfig.getSerializer();
-    this.snapshotComparator = snapshotConfig.getComparator();
-    this.snapshotReporters = snapshotConfig.getReporters();
-    this.scenario = null;
-  }
-
-  public void toMatchSnapshot() {
-
-    Set<Snapshot> rawSnapshots = snapshotFile.getSnapshots();
-    Snapshot previousSnapshot = getRawSnapshot(rawSnapshots);
-    Snapshot currentSnapshot = takeSnapshot();
-
-    if (previousSnapshot != null && shouldUpdateSnapshot()) {
-      snapshotFile.getSnapshots().remove(previousSnapshot);
-      previousSnapshot = null;
+        this.isCI = snapshotConfig.isCI();
+        this.snapshotSerializer = snapshotConfig.getSerializer();
+        this.snapshotComparator = snapshotConfig.getComparator();
+        this.snapshotReporters = snapshotConfig.getReporters();
+        this.scenario = null;
     }
 
-    if (previousSnapshot != null) {
-      snapshotFile.pushDebugSnapshot(currentSnapshot);
+    public void toMatchSnapshot() {
 
-      // Match existing Snapshot
-      if (System.getProperty("shadowMode") != null && "false".equals(System.getProperty("shadowMode"))) {
-        if (!snapshotComparator.matches(previousSnapshot, currentSnapshot)) {
-          snapshotFile.createDebugFile(currentSnapshot);
+        Set<Snapshot> rawSnapshots = snapshotFile.getSnapshots();
+        Snapshot previousSnapshot = getRawSnapshot(rawSnapshots);
+        Snapshot currentSnapshot = takeSnapshot();
 
-          List<SnapshotReporter> reporters =
-                  snapshotReporters.stream()
-                          .filter(reporter -> reporter.supportsFormat(snapshotSerializer.getOutputFormat()))
-                          .collect(Collectors.toList());
+        if (previousSnapshot != null && shouldUpdateSnapshot()) {
+            snapshotFile.getSnapshots().remove(previousSnapshot);
+            previousSnapshot = null;
+        }
 
-          if (reporters.isEmpty()) {
-            String comparator = snapshotComparator.getClass().getSimpleName();
-            throw new IllegalStateException(
-                    "No compatible reporters found for comparator " + comparator);
-          }
+        if (previousSnapshot != null) {
+            if (System.getProperty("shadowMode") != null && "false".equals(System.getProperty("shadowMode"))) {
+                snapshotFile.pushDebugSnapshot(currentSnapshot);
 
-          List<Throwable> errors = new ArrayList<>();
+                // Match existing Snapshot
+                if (!snapshotComparator.matches(previousSnapshot, currentSnapshot)) {
+                    snapshotFile.createDebugFile(currentSnapshot);
 
-          for (SnapshotReporter reporter : reporters) {
-            try {
-              reporter.report(previousSnapshot, currentSnapshot);
-            } catch (Throwable t) {
-              errors.add(t);
+                    List<SnapshotReporter> reporters =
+                            snapshotReporters.stream()
+                                    .filter(reporter -> reporter.supportsFormat(snapshotSerializer.getOutputFormat()))
+                                    .collect(Collectors.toList());
+
+                    if (reporters.isEmpty()) {
+                        String comparator = snapshotComparator.getClass().getSimpleName();
+                        throw new IllegalStateException(
+                                "No compatible reporters found for comparator " + comparator);
+                    }
+
+                    List<Throwable> errors = new ArrayList<>();
+
+                    for (SnapshotReporter reporter : reporters) {
+                        try {
+                            reporter.report(previousSnapshot, currentSnapshot);
+                        } catch (Throwable t) {
+                            errors.add(t);
+                        }
+                    }
+
+                    if (!errors.isEmpty()) {
+                        throw new SnapshotMatchException("Error(s) matching snapshot(s)", errors);
+                    }
+                }
             }
-          }
-
-          if (!errors.isEmpty()) {
-            throw new SnapshotMatchException("Error(s) matching snapshot(s)", errors);
-          }
+        } else {
+            if (this.isCI) {
+                log.error(
+                        "We detected you are running on a CI Server - if this is incorrect please override the isCI() method in SnapshotConfig");
+                throw new SnapshotMatchException(
+                        "Snapshot ["
+                                + resolveSnapshotIdentifier()
+                                + "] not found. Has this snapshot been committed ?");
+            } else {
+                log.warn(
+                        "We detected you are running on a developer machine - if this is incorrect please override the isCI() method in SnapshotConfig");
+                // Create New Snapshot
+                snapshotFile.pushSnapshot(currentSnapshot);
+                snapshotFile.pushDebugSnapshot(currentSnapshot);
+            }
         }
-      }
-    } else {
-      if (this.isCI) {
-        log.error(
-            "We detected you are running on a CI Server - if this is incorrect please override the isCI() method in SnapshotConfig");
-        throw new SnapshotMatchException(
-            "Snapshot ["
-                + resolveSnapshotIdentifier()
-                + "] not found. Has this snapshot been committed ?");
-      } else {
-        log.warn(
-            "We detected you are running on a developer machine - if this is incorrect please override the isCI() method in SnapshotConfig");
-        // Create New Snapshot
-        snapshotFile.pushSnapshot(currentSnapshot);
-        snapshotFile.pushDebugSnapshot(currentSnapshot);
-      }
     }
-  }
 
-  private boolean shouldUpdateSnapshot() {
-    if (snapshotConfig.updateSnapshot().isPresent() && snapshotConfig.isCI()) {
-      throw new SnapshotExtensionException(
-          "isCI=true & update-snapshot="
-              + snapshotConfig.updateSnapshot()
-              + ". Updating snapshots on CI is not allowed");
-    }
-    if (snapshotConfig.updateSnapshot().isPresent()) {
-      return resolveSnapshotIdentifier().contains(snapshotConfig.updateSnapshot().get());
-    } else {
-      return false;
-    }
-  }
-
-  private Snapshot getRawSnapshot(Collection<Snapshot> rawSnapshots) {
-    synchronized (rawSnapshots) {
-      for (Snapshot rawSnapshot : rawSnapshots) {
-        if (rawSnapshot.getIdentifier().equals(resolveSnapshotIdentifier())) {
-          return rawSnapshot;
+    private boolean shouldUpdateSnapshot() {
+        if (snapshotConfig.updateSnapshot().isPresent() && snapshotConfig.isCI()) {
+            throw new SnapshotExtensionException(
+                    "isCI=true & update-snapshot="
+                            + snapshotConfig.updateSnapshot()
+                            + ". Updating snapshots on CI is not allowed");
         }
-      }
+        if (snapshotConfig.updateSnapshot().isPresent()) {
+            return resolveSnapshotIdentifier().contains(snapshotConfig.updateSnapshot().get());
+        } else {
+            return false;
+        }
     }
-    return null;
-  }
 
-  private Snapshot takeSnapshot() {
-    SnapshotSerializerContext sg = SnapshotSerializerContext.from(this);
-    return snapshotSerializer.apply(current, sg);
-  }
-
-  String resolveSnapshotIdentifier() {
-    String scenarioFormat = scenario == null ? "" : "[" + scenario + "]";
-    return snapshotName() + scenarioFormat;
-  }
-
-  private String snapshotName() {
-    SnapshotName snapshotName = testMethod.getAnnotation(SnapshotName.class);
-    return snapshotName == null
-        ? testClass.getName() + "." + testMethod.getName()
-        : snapshotName.value();
-  }
-
-  void checkValidContext() {
-    for (String rw : RESERVED_WORDS) {
-      if (snapshotName().contains(rw)) {
-        throw new ReservedWordException("snapshot name", rw, RESERVED_WORDS);
-      }
-      if (scenario != null && scenario.contains(rw)) {
-        throw new ReservedWordException("scenario name", rw, RESERVED_WORDS);
-      }
+    private Snapshot getRawSnapshot(Collection<Snapshot> rawSnapshots) {
+        synchronized (rawSnapshots) {
+            for (Snapshot rawSnapshot : rawSnapshots) {
+                if (rawSnapshot.getIdentifier().equals(resolveSnapshotIdentifier())) {
+                    return rawSnapshot;
+                }
+            }
+        }
+        return null;
     }
-  }
+
+    private Snapshot takeSnapshot() {
+        SnapshotSerializerContext sg = SnapshotSerializerContext.from(this);
+        return snapshotSerializer.apply(current, sg);
+    }
+
+    String resolveSnapshotIdentifier() {
+        String scenarioFormat = scenario == null ? "" : "[" + scenario + "]";
+        return snapshotName() + scenarioFormat;
+    }
+
+    private String snapshotName() {
+        SnapshotName snapshotName = testMethod.getAnnotation(SnapshotName.class);
+        return snapshotName == null
+                ? testClass.getName() + "." + testMethod.getName()
+                : snapshotName.value();
+    }
+
+    void checkValidContext() {
+        for (String rw : RESERVED_WORDS) {
+            if (snapshotName().contains(rw)) {
+                throw new ReservedWordException("snapshot name", rw, RESERVED_WORDS);
+            }
+            if (scenario != null && scenario.contains(rw)) {
+                throw new ReservedWordException("scenario name", rw, RESERVED_WORDS);
+            }
+        }
+    }
 }
