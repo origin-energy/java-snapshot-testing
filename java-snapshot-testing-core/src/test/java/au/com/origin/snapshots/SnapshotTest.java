085b68b173beacf911d188a261d4c241c500bc18
Roger Viñas <roger.vinas@n26.com>
2024-11-29 10:32:52 +0100

    Add SnapshotTest

---
+++ java-snapshot-testing-core/src/test/java/au/com/origin/snapshots/SnapshotTest.java
@@ -0,0 +1,100 @@
+package au.com.origin.snapshots;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class SnapshotTest {
+
+    /*
+     name[scenario]={
+        "header-key": "header-value"
+      }body
+     */
+
+    @Test
+    public void shouldParseSnapshot() {
+        Snapshot snapshot = Snapshot.parse(Snapshot.builder()
+                .name("au.com.origin.snapshots.Test")
+                .body("body")
+                .build()
+                .raw()
+        );
+        assertThat(snapshot.getIdentifier()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getName()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getHeader()).isEmpty();
+        assertThat(snapshot.getScenario()).isBlank();
+        assertThat(snapshot.getBody()).isEqualTo("body");
+    }
+
+    @Test
+    public void shouldParseSnapshotWithHeaders() {
+        SnapshotHeader header = new SnapshotHeader();
+        header.put("header1", "value1");
+        Snapshot snapshot = Snapshot.parse(Snapshot.builder()
+                .name("au.com.origin.snapshots.Test")
+                .header(header)
+                .body("body")
+                .build()
+                .raw()
+        );
+        assertThat(snapshot.getIdentifier()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getName()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getHeader()).containsExactly(entry("header1", "value1"));
+        assertThat(snapshot.getScenario()).isBlank();
+        assertThat(snapshot.getBody()).isEqualTo("body");
+    }
+
+    @Test
+    public void shouldParseSnapshotWithScenario() {
+        Snapshot snapshot = Snapshot.parse(Snapshot.builder()
+                .name("au.com.origin.snapshots.Test")
+                .scenario("scenario")
+                .body("body")
+                .build()
+                .raw()
+        );
+        assertThat(snapshot.getIdentifier()).isEqualTo("au.com.origin.snapshots.Test[scenario]");
+        assertThat(snapshot.getName()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getHeader()).isEmpty();
+        assertThat(snapshot.getScenario()).isEqualTo("scenario");
+        assertThat(snapshot.getBody()).isEqualTo("body");
+    }
+
+    @Test
+    public void shouldParseSnapshotWithScenarioAndHeaders() {
+        SnapshotHeader header = new SnapshotHeader();
+        header.put("header1", "value1");
+        Snapshot snapshot = Snapshot.parse(Snapshot.builder()
+                .name("au.com.origin.snapshots.Test")
+                .scenario("scenario")
+                .header(header)
+                .body("body")
+                .build()
+                .raw()
+        );
+        assertThat(snapshot.getIdentifier()).isEqualTo("au.com.origin.snapshots.Test[scenario]");
+        assertThat(snapshot.getName()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getHeader()).containsExactly(entry("header1", "value1"));
+        assertThat(snapshot.getScenario()).isEqualTo("scenario");
+        assertThat(snapshot.getBody()).isEqualTo("body");
+    }
+
+    @Test
+    public void shouldParseSnapshotWithScenarioAndBodyWithSomethingSimilarToAnScenarioToConfuseRegex() {
+        Snapshot snapshot = Snapshot.parse(Snapshot.builder()
+                .name("au.com.origin.snapshots.Test")
+                .scenario("scenario")
+                .body("[xxx]=yyy")
+                .build()
+                .raw()
+        );
+        System.out.println(snapshot.raw());
+        assertThat(snapshot.getIdentifier()).isEqualTo("au.com.origin.snapshots.Test[scenario]");
+        assertThat(snapshot.getName()).isEqualTo("au.com.origin.snapshots.Test");
+        assertThat(snapshot.getHeader()).isEmpty();
+        assertThat(snapshot.getScenario()).isEqualTo("scenario");
+        assertThat(snapshot.getBody()).isEqualTo("[xxx]=yyy");
+    }
+}
